ff4a7848cf691f4bf5a65e95dcadaa09fbd3f213
HarryHaw1234 <133182546+HarryHaw1234@users.noreply.github.com>
2023-05-22 08:59:56 +0630

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Main  from './components/Main'
+import Nav from './components/Nav';
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-
+  const [darkMode, setDarkMode] = useState(false)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
   return (
     <div>
-      <Main />
+      <Nav darkMode = {darkMode} toggleDarkMode = {toggleDarkMode}/>
+      <Main darkMode = {darkMode}/>
     </div>
 )
 }
